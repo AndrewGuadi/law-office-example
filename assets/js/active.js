27f0044f2ceeae89c9652c7ca1bb17cf3a2b698a
Andrew Guadí <94252760+AndrewGuadi@users.noreply.github.com>
2024-01-05 13:46:47 -0500

    initial commit

---
+++ assets/js/active.js
@@ -0,0 +1,21 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        $(".homepage-sliders").owlCarousel({
+            items: 1,
+            nav: true,
+            dots: false,
+            autoplay: true,
+            loop: true,
+            navText: ["<i class='fa fa-angle-right'></i>", "<i class='fa fa-angle-left'></i>"]
+          
+        });
+	});
+	
+	jQuery(window).load(function(){
+	
+	
+	});
+	
+}(jQuery));	
